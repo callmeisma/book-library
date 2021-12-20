830c5fb6b5a844804e0a74baa9992ab17d3d9c24
callmeisma <ismael.fdez87@gmail.com>
2021-12-20 11:02:49 -0500

    improved logic and front-end

--- script.js
+++ script.js
@@ -1,183 +1,139 @@
+// Database
 let myLibrary = [
-    {
-        title:'The Hobbit',
-        author: 'J.R.R.Tolkien',
-        pages: 295,
-        read: false,
-    },
-    {
-        title: 'Rich Dad Poor Dad',
-        author: 'Robert T. Kiyosaki',
-        pages: 336,
-        read: true,
-    },{
-        title: 'Money, Master the Game',
-        author: 'Tony Robbins',
-        pages: 688,
-        read: true,
-    }];
+  {
+    title: "Money, Master the Game",
+    author: "Tony Robbins",
+    read: true,
+  },
+  {
+    title: "Rich Dad Poor Dad",
+    author: "Rober Kiyosaki",
+    read: true,
+  },
+  {
+    title: `Harry Potter and the Philosopher's Stone `,
+    author: "J.K. Rowling",
+    read: false,
+  },
+];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    // this.info = function() {
-    //     if (this.read === 'Yes') {
-    //         return `${title} by ${author}, ${pages} pages, read.`;
-    //     } else {
-    //         return `${title} by ${author}, ${pages} pages, not read yet.`;
-    //     }
-    // }
+//  Data Creation
+function Book(title, author, read) {
+  this.title = title;
+  this.author = author;
+  this.read = read;
 }
 
-function addBooktoLibrary() {
-    title = document.getElementById("title").value;
-    author = document.getElementById("author").value;
-    pages = document.getElementById("pages").value;
-    read = document.getElementById("read").checked;
-
-    switch (true) {
-        case (title === ''):
-            alert('Please add a Title');
-            break;
-        case (author === ''):
-            alert('Please add an Author');
-            break;
-        case (pages === ''):
-            alert('Please add amount of pages');
-            break;
-        default:
-            myLibrary.push(new Book(title, author, pages, read));
-            cardMaker();
-            clearFields();
-            document.getElementById('collap').style.display = 'none';
-    }
-}
-
+// Form Functions
 function clearFields() {
-    document.getElementById("title").value = '';
-    document.getElementById("author").value = '';
-    document.getElementById("pages").value = '';
-    document.getElementById("read").checked = '';
+  document.getElementById("titleInput").value = "";
+  document.getElementById("authorInput").value = "";
+  document.getElementById("readInput").value = "";
 }
 
-function readToogle(item, status) {
-    myLibrary[item].read = status;
+function addBook() {
+  title = document.getElementById("titleInput").value;
+  author = document.getElementById("authorInput").value;
+  read = document.getElementById("readInput").value;
+  read = read == "true" ? true : false;
+
+  myLibrary.push(new Book(title, author, read));
+  render();
+
+  var collapseElementList = [].slice.call(
+    document.querySelectorAll(".collapse")
+  );
+  var collapseList = collapseElementList.map(function (collapseEl) {
+    return new bootstrap.Collapse(collapseEl);
+  });
+
+  clearFields();
+}
+// List Functions
+function readToogle(id) {
+  let currStatus = myLibrary[id].read;
+  myLibrary[id].read = !currStatus;
+  render();
 }
 
-function rmvBookfromLibrary(id) {
-    book = document.getElementById(id);
-    shelf = document.getElementById('shelf');
-    shelf.removeChild(book);
+function rmvBook(id) {
+  let book = document.getElementById(id);
+  myLibrary.splice(book.id, 1);
+  render();
 }
 
-function cardMaker () {
-    length = myLibrary.length - 1;
-    
-    bookContainer = document.getElementById('shelf');
-    bookItem = document.createElement('div');
-
-    bookItem.setAttribute('class', 'bookItem');
-    bookItem.setAttribute('id', length);
-
-    bookTitle = document.createElement('h3');
-    bookTitle.innerHTML = myLibrary[length].title;
-    bookItem.appendChild(bookTitle);
-
-    bookAuthor = document.createElement('p');
-    bookAuthor.innerHTML = myLibrary[length].author;
-    bookItem.appendChild(bookAuthor);
-
-    bookPages = document.createElement('p');
-    bookPages.innerHTML = myLibrary[length].pages;
-    bookItem.appendChild(bookPages);
-
-    bookRead = document.createElement('p');
-    bookRead.innerHTML = 'Read';
-    bookItem.appendChild(bookRead);
-
-    bookReadStat = document.createElement('input');
-    bookReadStat.setAttribute('type', 'checkbox');
-    bookReadStat.setAttribute('id', 'checkbox');
-    if (myLibrary[length].read === true) {
-        bookRead.setAttribute('checked','');
-        bookItem.appendChild(bookReadStat);
-    } else {
-        bookItem.appendChild(bookReadStat);
-    }
-
-    bookRmv = document.createElement('button');
-    bookRmv.setAttribute('id', 'rmv');
-    bookRmv.innerHTML = 'Remove';
-    bookItem.appendChild(bookRmv);
-
-
-    bookContainer.appendChild(bookItem);
-}
-
+// Render
 function render() {
-    if (myLibrary.length > 0) {
-        for (i = 0; i < myLibrary.length; i++) {
-            bookContainer = document.getElementById('shelf');
-            bookItem = document.createElement('div');
+  bookContainer = document.getElementById("shelf");
+  bookContainer.innerHTML = "";
 
-            bookItem.setAttribute('class', 'bookItem');
-            bookItem.setAttribute('id', i);
+  if (myLibrary.length > 0) {
+    for (i = 0; i < myLibrary.length; i++) {
+      // Book
+      bookItem = document.createElement("li");
+      bookItem.setAttribute("class", "card m-2 flex-fill");
+      bookItem.setAttribute("id", i);
+      // Book Cover
+      bookCover = document.createElement("span");
+      bookCover.setAttribute("class", "card-img-top placeholder p-5");
+      bookCover.setAttribute("style", "height: 12rem;");
+      bookItem.appendChild(bookCover);
+      // Book Body
+      bookBody = document.createElement("div");
+      bookBody.setAttribute("class", "card-body");
+      // Book Title
+      bookTitle = document.createElement("h5");
+      bookTitle.setAttribute("class", "card-title");
+      bookTitle.setAttribute("class", "m-0 p-0");
+      bookTitle.innerHTML = myLibrary[i].title;
+      bookBody.appendChild(bookTitle);
+      // Book Author
+      bookAuthor = document.createElement("h6");
+      bookAuthor.setAttribute("class", "card-subtitle text-muted m-0 p-0");
+      bookAuthor.innerHTML = myLibrary[i].author;
+      bookBody.appendChild(bookAuthor);
+      // Book Actions
+      bookActions = document.createElement("div");
+      bookActions.setAttribute("class", "m-0 p-0 d-flex justify-content-end");
+      // Read
+      bookRead = document.createElement("button");
+      bookRead.setAttribute("id", "status");
+      if (myLibrary[i].read === true) {
+        bookRead.setAttribute("class", "btn btn-success m-1");
+        bookRead.innerHTML = "Read";
+        bookActions.appendChild(bookRead);
+      } else if (myLibrary[i].read === false) {
+        bookRead.setAttribute("class", "btn btn-danger m-1");
+        bookRead.innerHTML = "Unread";
+        bookActions.appendChild(bookRead);
+      }
+      // Delete
+      bookRmvContainer = document.createElement("button");
+      bookRmvContainer.setAttribute("class", "btn btn-outline-primary m-1");
+      bookRmvContainer.setAttribute("id", "rmv");
+      bookRmv = document.createElement("i");
+      bookRmv.setAttribute("class", "bi bi-trash");
+      bookRmvContainer.appendChild(bookRmv);
+      bookActions.appendChild(bookRmvContainer);
+      bookBody.appendChild(bookActions);
+      bookItem.appendChild(bookBody);
 
-            bookTitle = document.createElement('h3');
-            bookTitle.innerHTML = myLibrary[i].title;
-            bookItem.appendChild(bookTitle);
-
-            bookAuthor = document.createElement('p');
-            bookAuthor.innerHTML = myLibrary[i].author;
-            bookItem.appendChild(bookAuthor);
-
-            bookPages = document.createElement('p');
-            bookPages.innerHTML = myLibrary[i].pages;
-            bookItem.appendChild(bookPages);
-
-            bookRead = document.createElement('p');
-            bookRead.innerHTML = 'Read';
-            bookItem.appendChild(bookRead);
-
-            bookReadStat = document.createElement('input');
-            bookReadStat.setAttribute('type', 'checkbox');
-            bookReadStat.setAttribute('id', 'checkbox');
-            if (myLibrary[i].read === true) {
-                bookReadStat.setAttribute('checked','');
-                bookItem.appendChild(bookReadStat);
-            } else {
-                bookItem.appendChild(bookReadStat);
-            }
-            
-            bookRmv = document.createElement('button');
-            bookRmv.setAttribute('id', 'rmv');
-            bookRmv.innerHTML = 'Remove';
-            bookItem.appendChild(bookRmv);
-
-            bookContainer.appendChild(bookItem);
-        }
+      bookContainer.appendChild(bookItem);
     }
-}   
-
-function collapse() {
-    form = document.getElementById('collap');
-    if (form.style.display === 'none') {
-        form.setAttribute('style', 'display: block')
-    } else if (form.style.display === 'block') {
-        form.setAttribute('style', 'display: none')
-    }
+  }
 }
-
 render();
 
-document.addEventListener('click', function(e) {
-    if (e.target.id === 'add') {
-        collapse();
-    } else if (e.target.id === 'rmv') {
-        rmvBookfromLibrary(e.path[1].id);
-    } else if (e.target.checked != undefined && e.target.id === 'checkbox') {
-        readToogle(e.target.parentNode.id, e.target.checked);
-    }
-})
+// Listeners
+document.addEventListener("click", function (e) {
+  if (e.target.id === "rmv") {
+    rmvBook(e.path[3].id);
+  } else if (e.target.id === "status") {
+    readToogle(e.path[3].id);
+  }
+  console.table(myLibrary);
+});
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+});
